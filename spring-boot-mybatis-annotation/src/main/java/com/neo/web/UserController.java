17c8d4409c9720d3af14b6f26a782c0fb68e459a
qfttxq <qfttxq@163.com>
2017-09-21 23:29:30 +0800

    abc

--- spring-boot-mybatis-annotation/src/main/java/com/neo/web/UserController.java
+++ spring-boot-mybatis-annotation/src/main/java/com/neo/web/UserController.java
@@ -25,6 +25,7 @@ public class UserController {
     @RequestMapping("/getUser")
     public UserEntity getUser(Long id) {
     	UserEntity user=userMapper.getOne(id);
+    	System.out.println(1111);
         return user;
     }
     
